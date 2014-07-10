a3662d9dda984d998514945f8af049eb92037fd2
Pranav Kotecha <p.kotecha@samsung.com>
2014-07-10 16:15:55 -0700

    Unit 2, Day 2

--- src/com/pranav/kilobolt/StartingClass.java
+++ src/com/pranav/kilobolt/StartingClass.java
@@ -1,19 +1,28 @@
 package com.pranav.kilobolt;
 
 import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
 
-public class StartingClass extends Applet {
+public class StartingClass extends Applet implements Runnable {
 
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
 		super.init();
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
 	}
 
 	@Override
 	public void start() {
 		// TODO Auto-generated method stub
 		super.start();
+		Thread thread = new Thread(this);
+		thread.start();
 	}
 
 	@Override
@@ -26,6 +35,19 @@ public class StartingClass extends Applet {
 	public void destroy() {
 		// TODO Auto-generated method stub
 		super.destroy();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		repaint();
+		while (true) {
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
