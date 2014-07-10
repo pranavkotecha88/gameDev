ccd897d26a3cbfebab49f8d4b38b6f8b6e5b6e5a
Pranav Kotecha <p.kotecha@samsung.com>
2014-07-10 16:36:53 -0700

    Unit 2, Day 3

--- src/com/pranav/kilobolt/StartingClass.java
+++ src/com/pranav/kilobolt/StartingClass.java
@@ -3,8 +3,11 @@ package com.pranav.kilobolt;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.RenderingHints.Key;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class StartingClass extends Applet implements Runnable {
+public class StartingClass extends Applet implements Runnable, KeyListener{
 
 	@Override
 	public void init() {
@@ -13,6 +16,7 @@ public class StartingClass extends Applet implements Runnable {
 		setSize(800, 480);
 		setBackground(Color.BLACK);
 		setFocusable(true);
+		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Q-Bot Alpha");
 	}
@@ -48,6 +52,58 @@ public class StartingClass extends Applet implements Runnable {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch(e.getKeyCode()) {
+		
+		case KeyEvent.VK_UP:
+			System.out.println("Move up");
+			break;
+		case KeyEvent.VK_DOWN:
+			System.out.println("Move Down");
+			break;
+		case KeyEvent.VK_LEFT:
+			System.out.println("Move left");
+			break;
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Move right");
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println("Jump");
+			break;
+		} // switch ends
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch(e.getKeyCode()) {
+		
+		case KeyEvent.VK_UP:
+			System.out.println("Stop Moving Up");
+			break;
+		case KeyEvent.VK_DOWN:
+			System.out.println("Stop Moving Down");
+			break;
+		case KeyEvent.VK_LEFT:
+			System.out.println("Stop Moving Left");
+			break;
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Stop Moving Right");
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println("Stop Jump");
+			break;
+		} // switch ends
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
